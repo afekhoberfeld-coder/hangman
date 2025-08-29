66ae3859094e0225f9e7945adfd98a019de26482
afekhoberfeld-coder <afek.hoberfeld@gmail.com>
2025-08-29 19:26:36 +0300

    FIRST COMMIT
    guessing works

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,29 @@
-word_file = open("words.txt","r")
-read_words = word_file.readline()
-word_list = []
-for line in word_list:
-    word_list.append(line)
-
-print("Hangman started")
-print(word_list)
-print("ilay dumb")
+WELCOME_WORD = """
+__        __   _                            _         
+\ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___   
+ \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  
+  \ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | 
+   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/  
+"""
+print(WELCOME_WORD)
+hangman_word = """
+ _    _          _   _  _____ __  __          _   _ 
+| |  | |   /\   | \ | |/ ____|  \/  |   /\   | \ | |
+| |__| |  /  \  |  \| | |  __| \  / |  /  \  |  \| |
+|  __  | / /\ \ | . ` | | |_ | |\/| | / /\ \ | . ` |
+| |  | |/ ____ \| |\  | |__| | |  | |/ ____ \| |\  |
+|_|  |_/_/    \_\_| \_|\_____|_|  |_/_/    \_\_| \_|
+"""
+print(hangman_word)
+word = "cat"
+def get_guess():
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if not len(guess) == 1:
+            print("single letter pls")
+            continue
+        if not guess.isalpha():
+            print("no number only a single letter")
+            continue
+        return guess
+print(f"that is a letter {get_guess()}")
--- testing.py
+++ testing.py
@@ -2,3 +2,6 @@ def code_working():
     print("succeful")
 
 code_working()
+for name in range(1,4):
+    print(f"{name} try" + (name * "."))
+print(range(3))
